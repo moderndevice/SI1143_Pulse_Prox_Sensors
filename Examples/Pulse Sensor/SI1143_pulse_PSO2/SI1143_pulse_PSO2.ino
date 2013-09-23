8b6ae49298a93074a317946f24c82b66c129f9b5
paulbadger <paul@moderndevice.com>
2013-09-22 21:53:24 -0400

    PSO2 example added

--- Examples/Pulse Sensor/SI1143_pulse_PSO2/SI1143_pulse_PSO2.ino
+++ Examples/Pulse Sensor/SI1143_pulse_PSO2/SI1143_pulse_PSO2.ino
@@ -1,8 +1,14 @@
-/* SI114_Pulse_Demo.ino
+/* SI114_pulse_PSO2.ino 
+ * A version of SI114_Pulse_Demo.ino with PSO2 sensing added
  * demo code for the Modern Device SI1143-based pulse sensor
  * http://moderndevice.com/product/pulse-heartbeat-sensor-pulse-sensor-1x/
  * Paul Badger 2013 with plenty of coding help from Jean-Claude Wippler
  * Hardware setup - please read the chart below and set the appropriate options
+ * See the #defines for various printing options
+ * This is experimental software (and pre-beta) at that, it is not suitable 
+ * for any particular purpose. No life-critical devices should
+ * be based on this software.
+ * Code in the public domain but credit for the software is nice :)
  */
  
  const int portForSI114 = 3;        // change to the JeeNode port number used
@@ -28,12 +34,12 @@ const int SAMPLES_TO_AVERAGE = 5;             // samples for smoothing 1 to 10 s
 // increase for smoother waveform (with less resolution - slower!) 
 
 
- #define SEND_TOTAL_TO_PROCESSING   // Use this option exclusive of other options
+//#define SEND_TOTAL_TO_PROCESSING   // Use this option exclusive of other options
                                       // for sending data to HeartbeatGraph in Processing
 // #define POWERLINE_SAMPLING         // samples on an integral of a power line period [eg 1/60 sec]
 // #define AMBIENT_LIGHT_SAMPLING     // also samples ambient slight (slightly slower)
 // #define PRINT_LED_VALS             // print LED raw values
-// #define GET_PULSE_READING          // prints HB and signal size
+ #define GET_PULSE_READING            // prints HB, signal size, PSO2 ratio
 
 
 int binOut;     // 1 or 0 depending on state of heartbeat
@@ -41,8 +47,9 @@ int BPM;
 unsigned long red;        // read value from visible red LED
 unsigned long IR1;        // read value from infrared LED1
 unsigned long IR2;       // read value from infrared LED2
-unsigned long total;     // all three LED reads added together
-int signalSize;          // the heartbeat signal minus the offset
+unsigned long IR_total;     // IR LED reads added together
+
+int IR_signalSize;          // the heartbeat signal minus the offset
 
 PortI2C myBus (portForSI114);
 PulsePlug pulse (myBus); 
@@ -95,7 +102,7 @@ void initPulseSensor(){
     Serial.print("PART: "); 
     Serial.print(pulse.getReg(PulsePlug::PART_ID)); 
     Serial.print(" REV: "); 
-    Serial.print(pulse.getReg(PulsePlug::REV_ID)); 
+    Serial.println(pulse.getReg(PulsePlug::REV_ID)); 
     Serial.print(" SEQ: "); 
     Serial.println(pulse.getReg(PulsePlug::SEQ_ID)); 
 
@@ -109,15 +116,20 @@ void initPulseSensor(){
     // Current setting for LEDs pulsed while taking readings
     // PS_LED21  Setting for LEDs 1 & 2. LED 2 is high nibble
     // each LED has 16 possible (0-F in hex) possible settings
-    // read the 
-    pulse.setReg(PulsePlug::PS_LED21, 0x38);      // LED current for 2 (IR1 - high nibble) & LEDs 1 (red - low nibble) 
+    // see the SI114x datasheet.
+    
+    // These settings should really be autimated with feedback from output
+    // On my todo list but your patch is appreciated :)
+    // support at moderndevice dot com.
+    pulse.setReg(PulsePlug::PS_LED21, 0x39);      // LED current for 2 (IR1 - high nibble) & LEDs 1 (red - low nibble) 
     pulse.setReg(PulsePlug::PS_LED3, 0x02);       // LED current for LED 3 (IR2)
 
+/*  debug infor for the led currents
     Serial.print( "PS_LED21 = ");                                         
     Serial.println(pulse.getReg(PulsePlug::PS_LED21), BIN);                                          
     Serial.print("CHLIST = ");
     Serial.println(pulse.readParam(0x01), BIN);
-
+*/
     pulse.writeParam(PulsePlug::PARAM_CH_LIST, 0x77);         // all measurements on
 
     // increasing PARAM_PS_ADC_GAIN will increase the LED on time and ADC window
@@ -127,9 +139,10 @@ void initPulseSensor(){
 
 
     // You can select which LEDs are energized for each reading.
-    // The settings below turn on only the LED that "normally" would be read
-    // ie LED1 is pulsed and read first, then LED2 is pulsed and read etc.
-    pulse.writeParam(PulsePlug::PARAM_PSLED12_SELECT, 0x21);  // 21 = LED 2 & LED 1 (red) resp.                                                              
+    // The settings below (in the comments)
+    // turn on only the LED that "normally" would be read
+    // ie LED1 is pulsed and read first, then LED2 & LED3.
+    pulse.writeParam(PulsePlug::PARAM_PSLED12_SELECT, 0x21);  // 21 select LEDs 2 & 1 (red) only                                                               
     pulse.writeParam(PulsePlug::PARAM_PSLED3_SELECT, 0x04);   // 4 = LED 3 only
 
     // Sensors for reading the three LEDs
@@ -148,14 +161,16 @@ void initPulseSensor(){
 
 void readPulseSensor(){
 
-    static int foundNewFinger;
-    static  int valley=0, peak=0, smoothPeak, smoothValley, binOut, lastBinOut, BPM;
+    static int foundNewFinger, red_signalSize, red_smoothValley;
+    static int red_valley, red_Peak, red_smoothRedPeak, red_smoothRedValley, 
+               red_HFoutput, red_smoothPeak; // for PSO2 calc
+    static  int IR_valley=0, IR_peak=0, IR_smoothPeak, IR_smoothValley, binOut, lastBinOut, BPM;
     static unsigned long lastTotal, lastMillis,  valleyTime = millis(), lastValleyTime = millis(), peakTime = millis(), lastPeakTime=millis(), lastBeat, beat;
-    static float baseline, HFoutput, HFoutput2, shiftedOutput, LFoutput, hysterisis;
+    static float IR_baseline, red_baseline, IR_HFoutput, IR_HFoutput2, shiftedOutput, LFoutput, hysterisis;
 
     unsigned long total=0, start;
     int i=0;
-    int signalSize;
+    int IR_signalSize;
     red = 0;
     IR1 = 0;
     IR2 = 0;
@@ -189,7 +204,7 @@ void readPulseSensor(){
     red = red / i;  // get averages
     IR1 = IR1 / i;
     IR2 = IR2 / i;
-    total = red + IR1 + IR2;
+    total =  IR1 + IR2;  // red excluded
     
    
 
@@ -234,7 +249,7 @@ void readPulseSensor(){
     if (++foundNewFinger > 25) foundNewFinger = 25;   // prevent rollover 
 
     if ( foundNewFinger < 20){
-        baseline = total - 200;   // take a guess at the baseline to prime smooth filter
+        IR_baseline = total - 200;   // take a guess at the baseline to prime smooth filter
    Serial.println("found new finger");     
     }
     
@@ -244,55 +259,90 @@ void readPulseSensor(){
         // baseline is the moving average of the signal - the middle of the waveform
         // the idea here is to keep track of a high frequency signal, HFoutput and a 
         // low frequency signal, LFoutput
-        // The HF signal is shifted downward slightly (heartbeats are negative peaks)
-        // The high freq signal has some hysterisis added. When the HF signal is less than the 
-        // shifted LF signal, we have found a heartbeat.
-        baseline = smooth(total, 0.99, baseline);   // 
-        HFoutput = smooth((total - baseline), 0.2, HFoutput);    // recycling output - filter to slow down response
-        HFoutput2 = HFoutput + hysterisis;     
-        LFoutput = smooth((total - baseline), 0.95, LFoutput);
+        // The LF signal is shifted downward slightly downward (heartbeats are negative peaks)
+        // The high freq signal has some hysterisis added. 
+        // When the HF signal crosses the shifted LF signal (on a downward slope), 
+        // we have found a heartbeat.
+        IR_baseline = smooth(total, 0.99, IR_baseline);   // 
+        IR_HFoutput = smooth((total - IR_baseline), 0.2, IR_HFoutput);    // recycling output - filter to slow down response
+        
+        red_baseline = smooth(red, 0.99, red_baseline); 
+        red_HFoutput = smooth((red - red_HFoutput), 0.2, red_HFoutput);
+        
+        // beat detection is performed only on the IR channel so 
+        // fewer red variables are needed
+        
+        IR_HFoutput2 = IR_HFoutput + hysterisis;     
+        LFoutput = smooth((total - IR_baseline), 0.95, LFoutput);
         // heartbeat signal is inverted - we are looking for negative peaks
-        shiftedOutput = LFoutput - (signalSize * .05);
+        shiftedOutput = LFoutput - (IR_signalSize * .05);
 
-        // We need to be able to keep track of peaks and valleys to scale the output for 
-        // user convenience. Hysterisis is also scaled.
-        if (HFoutput  > peak) peak = HFoutput; 
-        if (peak > 1500) peak = 1500; 
-
+        if (IR_HFoutput  > IR_peak) IR_peak = IR_HFoutput; 
+        if (red_HFoutput  > red_Peak) red_Peak = red_HFoutput;
+        
+        // default reset - only if reset fails to occur for 1800 ms
         if (millis() - lastPeakTime > 1800){  // reset peak detector slower than lowest human HB
-            smoothPeak =  smooth((float)peak, 0.6, (float)smoothPeak);  // smooth peaks
-            peak = 0;
+            IR_smoothPeak =  smooth((float)IR_peak, 0.6, (float)IR_smoothPeak);  // smooth peaks
+            IR_peak = 0;
+            
+            red_smoothPeak =  smooth((float)red_Peak, 0.6, (float)red_smoothPeak);  // smooth peaks
+            red_Peak = 0;
+            
             lastPeakTime = millis();
         }
 
-        if (HFoutput  < valley)   valley = HFoutput;
-        if (valley < -1500) valley = -1500;
+        if (IR_HFoutput  < IR_valley)   IR_valley = IR_HFoutput;
+        if (red_HFoutput  < red_valley)   red_valley = red_HFoutput;
+        
+  /*      if (IR_valley < -1500){
+            IR_valley = -1500;  // ditto above
+            Serial.println("-1500");
+        } 
+        if (red_valley < -1500) red_valley = -1500;  // ditto above  */
 
-        if (millis() - lastValleyTime > 1800){  // reset valleys detector slower than lowest human HB
-            smoothValley =  smooth((float)valley, 0.6, (float)smoothValley);  // smooth valleys
-            valley = 0;
+
+
+        if (millis() - lastValleyTime > 1800){  // insure reset slower than lowest human HB
+            IR_smoothValley =  smooth((float)IR_valley, 0.6, (float)IR_smoothValley);  // smooth valleys
+            IR_valley = 0;
             lastValleyTime = millis();           
         }
 
-        signalSize = smoothPeak - smoothValley;  // this the size of the smoothed HF heartbeat signal
-        
+   //     IR_signalSize = IR_smoothPeak - IR_smoothValley;  // this the size of the smoothed HF heartbeat signal
+        hysterisis = constrain((IR_signalSize / 15), 35, 120) ;  // you might want to divide by smaller number
+                                                                // if you start getting "double bumps"
+            
         // Serial.print(" T  ");
-        // Serial.print(signalSize); 
+        // Serial.print(IR_signalSize); 
 
-        if(HFoutput2 < shiftedOutput){
+        if  (IR_HFoutput2 < shiftedOutput){
+            // found a beat - pulses are valleys
             lastBinOut = binOut;
             binOut = 1;
          //   Serial.println("\t1");
-            hysterisis = - constrain((signalSize / 15), 35, 120) ;   // you might want to divide by smaller number
-            // if you start getting "double bumps"
+            hysterisis = -hysterisis;
+            IR_smoothValley =  smooth((float)IR_valley, 0.99, (float)IR_smoothValley);  // smooth valleys
+            IR_signalSize = IR_smoothPeak - IR_smoothValley;
+            IR_valley = 0x7FFF;
+            
+            red_smoothValley =  smooth((float)red_valley, 0.99, (float)red_smoothValley);  // smooth valleys
+            red_signalSize = red_smoothPeak - red_smoothValley;
+            red_valley = 0x7FFF;
+            
+            lastValleyTime = millis();
+             
         } 
-        else {
+        else{
          //   Serial.println("\t0");
             lastBinOut = binOut;
             binOut = 0;
-            hysterisis = constrain((signalSize / 15), 35, 120);    // ditto above
+            IR_smoothPeak =  smooth((float)IR_peak, 0.99, (float)IR_smoothPeak);  // smooth peaks
+            IR_peak = 0;
             
-        } 
+            red_smoothPeak =  smooth((float)red_Peak, 0.99, (float)red_smoothPeak);  // smooth peaks
+            red_Peak = 0;
+            lastPeakTime = millis();      
+            } 
 
   if (lastBinOut == 1 && binOut == 0){
       Serial.println(binOut);
@@ -305,13 +355,14 @@ void readPulseSensor(){
             Serial.print(binOut);
             Serial.print("\t BPM ");
             Serial.print(BPM);  
-            Serial.print("\t signal size ");
-            Serial.println(signalSize);  
+            Serial.print("\t IR ");
+            Serial.print(IR_signalSize);
+            Serial.print("\t PSO2 ");
+            Serial.println(((float)red_baseline / (float)(IR_baseline/2)), 3);                     
         }
 
     }
  #endif
-
 }
 
 
